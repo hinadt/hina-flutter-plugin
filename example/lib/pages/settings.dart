2a5a9119d7174aad70baa8c7b00ff0184eab2f41
wangjianhua <wangjianhua@hinadt.com>
2025-04-23 16:51:59 +0800

    测试demo增加点击事件采集案例

--- example/lib/pages/settings.dart
+++ example/lib/pages/settings.dart
@@ -16,15 +16,27 @@ class _SettingsState extends State<Settings> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          child: Center(child: Container(height: 200,
+          child: Center(child: Container(height: 600,
             child: Column(children: [
               GestureDetector(key: HNElementKey('open_about_key', isIgnore: false), child: Text('open_about'), onTap: () {
                 Navigator.of(context).pushNamed('/about');
               }),
-              SizedBox(height: 80,),
+              SizedBox(height: 60,),
               GestureDetector(key: HNElementKey('back_home_key', isIgnore: false), child: Text('back_home'), onTap: () {
                 Navigator.of(context).pop();
               }),
+              SizedBox(height: 60,),
+              ListTile(key: HNElementKey('back_home_key', isIgnore: false), title: Text('back_home_listtile'), onTap: () {
+                Navigator.of(context).pop();
+              }),
+              SizedBox(height: 60,),
+              ElevatedButton(key: HNElementKey("s001", properties: {"obj": {"a":"b"}}),child: Text('back_home_ElevateButton'), onPressed: () {
+                Navigator.of(context).pop();
+              }),
+              SizedBox(height: 60,),
+              InkWell(key: HNElementKey("s001", properties: {"pro": "500"}),child: Text('back_home_InkWell'), onTap: () {
+                Navigator.of(context).pop();
+              },)
             ],)
           ),),
         )
